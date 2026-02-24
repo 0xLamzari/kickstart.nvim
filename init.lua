65779d3d166cc109368ce39fc3b6e445412ab4d0
o.lamzari <o.lamzari@logifuture.com>
2026-02-24 14:56:33 +0100

    telescope file pre-filter grep

--- init.lua
+++ init.lua
@@ -403,6 +403,11 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader>sc', builtin.commands, { desc = '[S]earch [C]ommands' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>sl', function()
+        vim.ui.input({ prompt = 'file type filter (*.*) > ' }, function(input)
+          if input then require('telescope.builtin').live_grep { glob_pattern = input } end
+        end)
+      end, { desc = 'Live Grep With File Filter' })
 
       -- This runs on LSP attach per buffer (see main LSP attach function in 'neovim/nvim-lspconfig' config for more info,
       -- it is better explained there). This is a little bit redundant, but we can switch off telescope for an optional
